603c9a842f964a2ab63f1cae03a2709c7977b0da
wendell.guimaraes <wendell.guimaraes@ntendencia.com.br>
2019-12-23 16:57:48 -0300

    comit unico

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="rocket" title="Início"
+        subtitle="Cadastro de recrutamento TropaNova.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção
+            de um recrutamento de soldados para tropa Nova desenvolvido em React!</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por
+            <strong> WENDELL</strong>
+        </span>
+    </footer>
